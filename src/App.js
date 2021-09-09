7b9583f6a8884e6c640879a8bc29c4f347ff3fbe
Alex <alexbachurin91@gmail.com>
2021-09-09 16:49:18 +0300

    html+css setup + input change logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-
+import React, { useState } from 'react'
+import data from './data'
+import Title from './Title';
 import './App.css';
 
 function App() {
+  //get array with random text from data
+  const [loremText, setLoremText] = useState(data);
+  //get value to track our input value
+  const [value, setValue] = useState(0)
+
+  //watch input changes
+  const handleChange = (e) => {
+    e.preventDefault();
+    const currentValue = e.target.value;
+    //prevent value to go below 0
+    if (currentValue < 0) {
+      setValue(0);
+    } else {
+      setValue(currentValue);
+    }
+  }
   return (
-    <div className="App">
-      hello
-    </div>
+    <section className="section-center">
+      <Title />
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={value}
+          onChange={handleChange}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        <p>hello</p>
+      </article>
+    </section>
   );
 }
 
---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <>
+            <h3>tired of boring lorem ipsum?</h3>
+        </>
+    )
+}
